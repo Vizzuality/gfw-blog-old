37387ab598fcf5b7e14c5af3862275dc26711bb6
Carlos Matallín <matallo@vizzuality.com>
2014-01-02 12:27:12 +0100

    fix navigation

--- wp-content/themes/gfw-blog/single.php
+++ wp-content/themes/gfw-blog/single.php
@@ -37,7 +37,6 @@ get_header(); ?>
             })();
         </script>
         <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-        <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
       </div>
     <?php endwhile; // end of the loop. ?>
 
