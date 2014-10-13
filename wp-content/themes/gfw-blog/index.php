32d2d6c9619527da5385b0ab7305c6e9ea211758
Miguel Barrenechea <miguel.barrenechea@simbiotica.es>
2014-10-13 11:35:27 +0200

    Localhost detect for redirection

--- wp-content/themes/gfw-blog/index.php
+++ wp-content/themes/gfw-blog/index.php
@@ -11,10 +11,14 @@
  * @package GFW blog
  */
 
-if ( ! $_COOKIE['accepted_v4'] ) {
-  wp_redirect( 'http://www.globalforestwatch.org/accept_terms?return_to=' . home_url(add_query_arg(array(), $wp->request)) );
-  exit;
+$localhost = (substr($_SERVER['REMOTE_ADDR'], 0, 4) == '127.' || $_SERVER['REMOTE_ADDR'] == '::1');
+if (!$localhost) {
+	if ( ! $_COOKIE['accepted_v4'] ) {
+	  wp_redirect( 'http://www.globalforestwatch.org/accept_terms?return_to=' . home_url(add_query_arg(array(), $wp->request)) );
+	  exit;
+	}    
 }
+
 
 get_header(); ?>
 
