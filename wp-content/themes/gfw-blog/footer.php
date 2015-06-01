d94f8cf19f6bf37939c611163474894ef8e4c708
Adrián Pérez <adrian@simbiotica.es>
2015-06-01 13:32:41 +0200

    Point to the proxied version of the assets

--- wp-content/themes/gfw-blog/footer.php
+++ wp-content/themes/gfw-blog/footer.php
@@ -15,7 +15,7 @@
 </html>
   <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
   <script type="text/javascript" src="http://cdn.jsdelivr.net/jquery.slick/1.3.15/slick.min.js"></script>  
-  <script id="loader-gfw" type="text/javascript" src="https://cdn.rawgit.com/simbiotica/gfw_assets/831ef6558cf2693357fb23829ef7e6acf7ebc840/src/header-loader.js" data-current=".shape-blog"></script>
+  <script id="loader-gfw" type="text/javascript" src="http://globalforestwatch.org/gfw-assets" data-current=".shape-blog"></script>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
