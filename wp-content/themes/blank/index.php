d632bde970795b848971f5a720b8d50c68d648c8
Adrián Pérez <adrian@simbiotica.es>
2015-09-04 13:26:40 +0200

    link new theme featured image in card

--- wp-content/themes/blank/index.php
+++ wp-content/themes/blank/index.php
@@ -13,7 +13,7 @@ get_header(); ?>
       <article id="post-<?php the_ID(); ?>" class="card">
         <?php if (has_post_thumbnail( $post->ID ) ): ?>
           <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-          <img src="<?php echo $image[0]; ?>">
+          <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><img src="<?php echo $image[0]; ?>"></a>
         <?php endif; ?>
         <header>
           <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
