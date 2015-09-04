e2877a421e9bda71d3675c6afc302b778b767650
Adrián Pérez <adrian@simbiotica.es>
2015-09-04 11:41:18 +0200

    New theme: show featured image

--- wp-content/themes/blank/index.php
+++ wp-content/themes/blank/index.php
@@ -11,6 +11,12 @@ get_header(); ?>
     <?php while (have_posts()) : the_post(); ?>
 
       <article id="post-<?php the_ID(); ?>" class="card">
+        <?php if (has_post_thumbnail( $post->ID ) ): ?>
+          <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+          <div class="featured-image" style="background-image: url('<?php echo $image[0]; ?>')">
+
+          </div>
+        <?php endif; ?>
         <header>
           <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
         </header>
