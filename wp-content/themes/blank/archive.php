787b62b57c0740928e2631a586fb1dba6d7943f8
Adrián Pérez <adrian@simbiotica.es>
2015-09-15 11:52:20 +0200

    category and search result cards layout

--- wp-content/themes/blank/archive.php
+++ wp-content/themes/blank/archive.php
@@ -34,26 +34,29 @@ get_header(); ?>
     </nav>
 
     <?php while (have_posts()) : the_post(); ?>
-    <article <?php post_class() ?>>
-      <header>
-        <h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-        <time datetime="<?php the_time('Y-m-d')?>"><?php the_time('l, F jS, Y') ?></time>
-      </header>
-      <?php the_content() ?>
-      <footer>
-        <?php the_tags('Tags: ', ', ', '<br />'); ?>
-        Posted in <?php the_category(', ') ?>
-        | <?php edit_post_link('Edit', '', ' | '); ?>
-        <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
-      </footer>
-    </article>
+      <article id="post-<?php the_ID(); ?>" class="card" data-link="<?php the_permalink() ?>">
+          <img src="http://gfw.blog.s3.amazonaws.com/2015/06/17360624152_79047c6646_k-300x199.png<?php /*echo $image[0];*/ ?>">
+        
+        <?php if (has_post_thumbnail( $post->ID ) ): ?>
+          <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+          <!-- <img src="http://gfw.blog.s3.amazonaws.com/2015/06/17360624152_79047c6646_k-300x199.png<?php /*echo $image[0];*/ ?>"> -->
+        <?php endif; ?>
+        <header>
+          <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+        </header>
+        <div class="content">
+          <footer>
+            <?php /*the_tags('Tags: ', ', ', '<br />'); */?> 
+            <?php the_category(', ') ?>
+            <span><?php the_time('F jS, Y') ?></span>
+            <?php /*edit_post_link('Edit', '', ' | '); */?>
+            <?php /* comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); */?>
+          </footer>
+        </div>
+      </article>
     <?php endwhile; ?>
-
-    <nav>
-      <div><?php next_posts_link('&laquo; Older Entries') ?></div>
-      <div><?php previous_posts_link('Newer Entries &raquo;') ?></div>
-    </nav>
-  </section>
+    <span class="navigation-dir"><?php posts_nav_link('','<span class="prev-p"></span>',''); ?></span>
+    <span class="navigation-dir"><?php posts_nav_link('','','<span class="prev-p next"></span>'); ?></span>
 
   <?php else :
 
--- wp-content/themes/blank/search.php
+++ wp-content/themes/blank/search.php
@@ -6,47 +6,49 @@
 
 get_header(); ?>
 
-  <div id="main" role="main">
-
+<div id="main" role="main">
   <?php if (have_posts()) : ?>
-
-    <h2>Search Results</h2>
-
-    <nav>
-      <div><?php next_posts_link('&laquo; Older Entries') ?></div>
-      <div><?php previous_posts_link('Newer Entries &raquo;') ?></div>
-    </nav>
-
     <?php while (have_posts()) : the_post(); ?>
 
-      <article <?php post_class() ?>>
-        <h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-        <time><?php the_time('l, F jS, Y') ?></time>
-
-        <footer>
-          <?php the_tags('Tags: ', ', ', '<br />'); ?> 
-          Posted in <?php the_category(', ') ?>
-          | <?php edit_post_link('Edit', '', ' | '); ?>
-          <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
-        </footer>
+      <article id="post-<?php the_ID(); ?>" class="card" data-link="<?php the_permalink() ?>">
+          <img src="http://gfw.blog.s3.amazonaws.com/2015/06/17360624152_79047c6646_k-300x199.png<?php /*echo $image[0];*/ ?>">
+        
+        <?php if (has_post_thumbnail( $post->ID ) ): ?>
+          <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+          <!-- <img src="http://gfw.blog.s3.amazonaws.com/2015/06/17360624152_79047c6646_k-300x199.png<?php /*echo $image[0];*/ ?>"> -->
+        <?php endif; ?>
+        <header>
+          <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+        </header>
+        <div class="content">
+          <footer>
+            <?php /*the_tags('Tags: ', ', ', '<br />'); */?> 
+            <?php the_category(', ') ?>
+            <span><?php the_time('F jS, Y') ?></span>
+            <?php /*edit_post_link('Edit', '', ' | '); */?>
+            <?php /* comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); */?>
+          </footer>
+        </div>
       </article>
-
     <?php endwhile; ?>
-
-    <nav>
-      <div><?php next_posts_link('&laquo; Older Entries') ?></div>
-      <div><?php previous_posts_link('Newer Entries &raquo;') ?></div>
-    </nav>
-
+    <span class="navigation-dir"><?php posts_nav_link('','<span class="prev-p"></span>',''); ?></span>
+    <span class="navigation-dir"><?php posts_nav_link('','','<span class="prev-p next"></span>'); ?></span>
   <?php else : ?>
 
-    <h2>No posts found. Try a different search?</h2>
+    <h2>Not Found</h2>
+    <p>Sorry, but you are looking for something that isn't here.</p>
     <?php get_search_form(); ?>
 
   <?php endif; ?>
-
-  </div>
+</div>
 
 <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
+
+
+<script type="text/javascript">
+$('.card:not(footer a)').on('click',function(){
+  location.href=$(this).data('link')
+})
+</script>
