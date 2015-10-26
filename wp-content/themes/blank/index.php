d66cae7472795b3286f40f1a5ebe5621d4b09545
Adrián Pérez <adrian@simbiotica.es>
2015-10-26 16:46:54 +0100

    test pictures

--- wp-content/themes/blank/index.php
+++ wp-content/themes/blank/index.php
@@ -11,11 +11,11 @@ get_header(); ?>
       <?php while (have_posts()) : the_post(); ?>
 
         <article id="post-<?php the_ID(); ?>" class="card" data-link="<?php the_permalink() ?>">
-            <img src="<?php $image[0]; ?>">
-          
           <?php if (has_post_thumbnail( $post->ID ) ): ?>
             <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
           <?php endif; ?>
+            <img src="<?php $image[0]; ?>">
+          
           <header>
             <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
           </header>
