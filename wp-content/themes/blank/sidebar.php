82f526548bcca57fa55d3b6d6a1bd2f9d96f3e77
adrian perez <adrianperez@berkley.local>
2016-02-09 16:45:10 +0100

    featured user

--- wp-content/themes/blank/sidebar.php
+++ wp-content/themes/blank/sidebar.php
@@ -14,8 +14,8 @@
     <span class="title">featured gfw user</span>
     <div class="f-user">
       <img src="http://gfw.blog.s3.amazonaws.com/2016/01/campaign-on-mangrove-protection.jpg">
-      <span class="name"><a href="http://blog.globalforestwatch.org/2016/1/gfw-user-profile-roger-ngoufo/" target="_blank">Roger Ngoufo</a></span>
-      <span class="name pro">President of Cameroon Environmental Watch</span>
+      <span class="name"><a href="http://blog.globalforestwatch.org/2016/02/gfw-user-profile-craig-leisher/" target="_blank">Craig Leisher</a></span>
+      <span class="name pro">Director of Monitoring and Evaluations in Africa</span>
     </div>
   </section>
   <section>
