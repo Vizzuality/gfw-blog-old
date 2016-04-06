879f8a9b5d681d829f9bc84dc76cc307b07a44cd
Adrián <aloha@adrianperez.info>
2016-04-06 18:07:30 +0200

    retrieve featured user from post

--- wp-content/themes/blank/sidebar.php
+++ wp-content/themes/blank/sidebar.php
@@ -11,11 +11,19 @@
     <?php get_search_form(); ?>
   </section>
   <section>
+    <?php
+      $featuredUser = get_page_by_title( 'FEATURED GFW USER TEMPLATE' );
+      $FeaturedUserPic = get_post_meta($featuredUser->ID, 'FeaturedUserPic',true);
+      $FeaturedUserUrl = get_post_meta($featuredUser->ID, 'FeaturedUserUrl',true);
+      $FeaturedUserName = get_post_meta($featuredUser->ID, 'FeaturedUserName',true);
+      $FeaturedUserPro = get_post_meta($featuredUser->ID, 'FeaturedUserPro',true);
+
+    ?>
     <span class="title">featured gfw user</span>
     <div class="f-user">
-      <img src="http://gfw.blog.s3.amazonaws.com/2016/03/Madi.png">
-      <span class="name"><a href="http://blog.globalforestwatch.org/2016/03/gfw-user-profile-madi-vorva/" target="_blank">Madi Vorva</a></span>
-      <span class="name pro">Student at Pomona College and long-time forest activist</span>
+      <img src="<?php echo $FeaturedUserPic; ?>">
+      <span class="name"><a href="<?php echo $FeaturedUserUrl; ?>" target="_blank"><?php echo $FeaturedUserName; ?></a></span>
+      <span class="name pro"><?php echo $FeaturedUserPro; ?></span>
     </div>
   </section>
   <section>
