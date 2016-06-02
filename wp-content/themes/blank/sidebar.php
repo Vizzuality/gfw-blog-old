02549be03b7af37933a52f9fe7b179b1a577cc7d
Adrián <aloha@adrianperez.info>
2016-06-02 19:37:59 +0200

    languages fake content

--- wp-content/themes/blank/sidebar.php
+++ wp-content/themes/blank/sidebar.php
@@ -43,9 +43,9 @@
           if ($category->count < 2) break;
           if ($max == null) {
             $max = $category->count;
-            echo '<li><span class="slug-content"><span style="width:100%;"><b>'.$category->name.'</b></span> <em>'.$category->count.'</em></span><input type="checkbox" value="'.$category->slug.'" id="tagoption-'.$category->slug.'" ><label for="tagoption-'.$category->slug.'"><span></span></label></li>';
+            echo '<li><span class="slug-content"><span style="width:100%;"><b>'.$category->name.'</b></span> <em>'.$category->count.'</em></span><input data-name="'.$category->name.'" type="checkbox" value="'.$category->slug.'" id="tagoption-'.$category->slug.'" ><label for="tagoption-'.$category->slug.'"><span></span></label></li>';
           } else {
-            echo '<li><span class="slug-content"><span style="width:'.($category->count*100)/$max.'%"><b>'.$category->name.'</b></span> <em>'.$category->count.'</em></span><input type="checkbox" value="'.$category->slug.'" id="tagoption-'.$category->slug.'" ><label for="tagoption-'.$category->slug.'"><span></span></label></li>';
+            echo '<li><span class="slug-content"><span style="width:'.($category->count*100)/$max.'%"><b>'.$category->name.'</b></span> <em>'.$category->count.'</em></span><input data-name="'.$category->name.'" type="checkbox" value="'.$category->slug.'" id="tagoption-'.$category->slug.'" ><label for="tagoption-'.$category->slug.'"><span></span></label></li>';
           }
           // echo '<div class="line-wrapper">';
           // if ($max == null) {
@@ -60,7 +60,14 @@
     </ul>
     <a class="title" id="toggleMoreTagsSidebar">More tags ▼</a>
   </section>
-  <section>
+  <section class="tags-list">
+        <span class="title">languages<!--<span class="slug-content all">Select all  <input type="checkbox" value="all_l" id="tagoption-all_l" ><label for="tagoption-all_l"><span></span></label></span> --></span>
+    <ul>
+      <li>
+        <span class="slug-content"><span style="width:100%;"><b>English</b></span> <em>13</em></span></li>
+        <li><span class="slug-content"><span style="width:70%;"><b>Spanish</b></span> <em>10</em></span></li>
+        <li><span class="slug-content"><span style="width:50%;"><b>French</b></span> <em>7</em></span></li>
+    </ul>
   </section>
 </aside>
 
