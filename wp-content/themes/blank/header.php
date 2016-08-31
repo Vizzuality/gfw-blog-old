42ab7fab37e949c40b93a820761920cc3b7632c6
Adrián <aloha@adrianperez.info>
2016-08-31 19:38:31 +0200

    gfw-community categories

--- wp-content/themes/blank/header.php
+++ wp-content/themes/blank/header.php
@@ -94,7 +94,13 @@
         </li>
         <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>fires/" class="<?php if (is_category() && in_category( 'fires' )) echo 'selected' ?>">fires</a></li>
         <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>climate/" class="<?php if (is_category() && in_category( 'climate' )) echo 'selected' ?>">climate</a></li>
-        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>gfw-community/" class="<?php if (is_category() && in_category( 'community' )) echo 'selected' ?>">gfw community</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>gfw-community/" class="<?php if (is_category() && in_category( 'community' )) echo 'selected' ?>">gfw community</a>
+        <ul>
+            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>gfw-community/partner posts">partner posts</a></li>
+            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>gfw-community/user-profiles">user profiles</a></li>
+            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>gfw-community/events">events</a></li>
+          </ul>
+        </li>
         <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/features/" class="<?php if (is_category() && in_category( 'features' )) echo 'selected' ?>">new features</a></li>
       </ul>
     </nav>
