3ede7e6e85da0a910984a643a8ed51c72fd452e6
Adrián <aloha@adrianperez.info>
2016-08-31 19:31:16 +0200

    supplychain typo

--- wp-content/themes/blank/header.php
+++ wp-content/themes/blank/header.php
@@ -84,7 +84,7 @@
       <ul>
         <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="<?php if (is_home()) echo 'selected' ?>">home</a></li>
         <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>data/" class="<?php if (is_category() && in_category( 'data' )) echo 'selected' ?>">data</a></li>
-        <li id="supply-chain-menu"><a href="<?php echo esc_url( home_url( '/' ) ); ?>suply-chain/" class="<?php if (is_category() && in_category( 'supplychain' )) echo 'selected' ?>">supply chain</a>
+        <li id="supply-chain-menu"><a href="<?php echo esc_url( home_url( '/' ) ); ?>supplychain/" class="<?php if (is_category() && in_category( 'supplychain' )) echo 'selected' ?>">supply chain</a>
           <ul>
             <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>supplychain/agriculture">agriculture</a></li>
             <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>supplychain/logging">logging</a></li>
@@ -92,18 +92,18 @@
             <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>supplychain/infrastructure">infrastructure</a></li>
           </ul>
         </li>
-        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>category/fires/" class="<?php if (is_category() && in_category( 'fires' )) echo 'selected' ?>">fires</a></li>
-        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>category/climate/" class="<?php if (is_category() && in_category( 'climate' )) echo 'selected' ?>">climate</a></li>
-        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>category/gfw-community/" class="<?php if (is_category() && in_category( 'community' )) echo 'selected' ?>">gfw community</a></li>
-        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>category/features/" class="<?php if (is_category() && in_category( 'features' )) echo 'selected' ?>">new features</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/fires/" class="<?php if (is_category() && in_category( 'fires' )) echo 'selected' ?>">fires</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/climate/" class="<?php if (is_category() && in_category( 'climate' )) echo 'selected' ?>">climate</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/gfw-community/" class="<?php if (is_category() && in_category( 'community' )) echo 'selected' ?>">gfw community</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/features/" class="<?php if (is_category() && in_category( 'features' )) echo 'selected' ?>">new features</a></li>
       </ul>
     </nav>
     <div id="mobile-sidebar">
       <ul>
-        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>category/update/">updates</a></li>
-        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>category/news/">news roundup</a></li>
-        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>category/feature/">featured posts</a></li>
-        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>category/mapoftheday/">map of the week</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/update/">updates</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/news/">news roundup</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/feature/">featured posts</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>/mapoftheday/">map of the week</a></li>
         <li><?php get_search_form(); ?></li>
       </ul>
     </div>
