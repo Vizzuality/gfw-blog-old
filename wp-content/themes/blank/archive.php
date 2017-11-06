096ff112403fa4815bae89c5dfc8da32097838d4
pjosh <jorgegar@gmail.com>
2017-11-06 12:58:42 +0100

    Stories section links

--- wp-content/themes/blank/archive.php
+++ wp-content/themes/blank/archive.php
@@ -8,7 +8,15 @@ get_header(); ?>
 
     <?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
     <?php /* If this is a category archive */ if (is_category()) { ?>
-    <h2 class="pagetitle">Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category</h2>
+    <h2 class="pagetitle">
+      Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category
+      <?php if (in_category( 'stories' )): ?>
+        <a href="http://www.globalforestwatch.org/stories/new" class="add-button">
+          <div class="add-button-text">Add your own story</div>
+          <div class="add-button-bubble">+</div>
+        </a>
+      <?php endif ?>
+    </h2>
     <?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
     <h2 class="pagetitle">Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h2>
     <?php /* If this is a daily archive */ } elseif (is_day()) { ?>
